c9bda5040c11621ec9a432644c384062ed41132a
mohit_celona <mohit@celona.io>
2021-11-24 14:37:19 -0800

    Generate proto descriptors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,21 @@
+import com.google.protobuf.gradle.builtins
+import com.google.protobuf.gradle.generateProtoTasks
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.tree-ware"
 version = "1.0-SNAPSHOT"
 
 val log4j2Version = "2.14.1"
+val protoVersion = "3.19.1"
 
 plugins {
     id("org.jetbrains.kotlin.jvm").version("1.5.21")
     id("idea")
     id("java-library")
     id("java-test-fixtures")
+    id("com.google.protobuf").version("0.8.17")
 }
 
 repositories {
@@ -24,11 +30,10 @@ tasks.withType<KotlinCompile> {
 
 dependencies {
     implementation(project(":tree-ware-kotlin-core"))
-
-    implementation(kotlin("stdlib"))
-
+    implementation("com.google.protobuf:protobuf-kotlin:$protoVersion")
     implementation("org.apache.logging.log4j:log4j-api:$log4j2Version")
     implementation("org.apache.logging.log4j:log4j-core:$log4j2Version")
+    implementation(kotlin("stdlib"))
 
     testImplementation(testFixtures(project(":tree-ware-kotlin-core")))
     testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4j2Version")
@@ -37,4 +42,20 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.19.1"
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.generateDescriptorSet = true
+            task.descriptorSetOptions.includeImports = true
+            task.builtins {
+                // TODO(mohit): figure out Kotlin syntax for the following Groovy syntax.
+//                remove(java)
+            }
+        }
+    }
 }
