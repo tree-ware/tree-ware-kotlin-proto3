03a08b70f8f6d2c8174cb14f178ecc4dff15d24f
Deepak Nulu <deepak@celona.io>
2021-12-02 18:12:15 -0800

    Validate protobuf enum mappings
    Resolves #15

--- src/main/kotlin/org/treeWare/proto3/validation/ValidateProto3MetaModelMap.kt
+++ src/main/kotlin/org/treeWare/proto3/validation/ValidateProto3MetaModelMap.kt
@@ -1,5 +1,6 @@
 package org.treeWare.proto3.validation
 
+import com.google.protobuf.DescriptorProtos
 import com.google.protobuf.DescriptorProtos.FileDescriptorSet
 import org.treeWare.metaModel.traversal.AbstractLeader1Follower0MetaModelVisitor
 import org.treeWare.metaModel.traversal.metaModelForEach
@@ -20,30 +21,44 @@ private class ValidateProto3MetaModelMapVisitor(
     protoDescriptorFile: String
 ) : AbstractLeader1Follower0MetaModelVisitor<TraversalAction>(TraversalAction.CONTINUE) {
     val errors = mutableListOf<String>()
-    private var entityPath = ""
+    private var parentPath = ""
     private val parsedProtoDescriptorMap: Map<String, Int> = parseProto(protoDescriptorFile)
     private val validatedAbsolutePaths = HashSet<String>()
 
-    override fun visitEntityMeta(leaderEntityMeta1: EntityModel): TraversalAction {
-        val aux = getProto3MetaModelMap(leaderEntityMeta1)
-        entityPath = aux?.path ?: ""
+    override fun visitEnumerationMeta(leaderEnumerationMeta1: EntityModel): TraversalAction =
+        visitParentMeta(leaderEnumerationMeta1)
+
+    override fun visitEnumerationValueMeta(leaderEnumerationValueMeta1: EntityModel): TraversalAction =
+        visitChildMeta(leaderEnumerationValueMeta1)
+
+    override fun visitEntityMeta(leaderEntityMeta1: EntityModel): TraversalAction =
+        visitParentMeta(leaderEntityMeta1)
+
+    override fun visitFieldMeta(leaderFieldMeta1: EntityModel): TraversalAction =
+        visitChildMeta(leaderFieldMeta1)
+
+    // Helpers
+
+    private fun visitParentMeta(parentMeta: EntityModel): TraversalAction {
+        val aux = getProto3MetaModelMap(parentMeta)
+        parentPath = aux?.path ?: ""
         return TraversalAction.CONTINUE
     }
 
-    override fun visitFieldMeta(leaderFieldMeta1: EntityModel): TraversalAction {
-        val aux = getProto3MetaModelMap(leaderFieldMeta1)
+    private fun visitChildMeta(childMeta: EntityModel): TraversalAction {
+        val aux = getProto3MetaModelMap(childMeta)
         // Check if field contains mapping to proto3 path
         if (aux?.path != null) {
-            val absolutePath = if (aux.path.contains(".proto:/")) aux.path else "$entityPath/${aux.path}"
+            val absolutePath = if (aux.path.contains(".proto:/")) aux.path else "$parentPath/${aux.path}"
             if (validatedAbsolutePaths.contains(absolutePath)) {
-                val fullName = leaderFieldMeta1.getAux<Resolved>(RESOLVED_AUX)?.fullName
+                val fullName = childMeta.getAux<Resolved>(RESOLVED_AUX)?.fullName
                 errors.add("$fullName has duplicate mapping $absolutePath")
             } else if (parsedProtoDescriptorMap.containsKey(absolutePath)) {
                 validatedAbsolutePaths.add(absolutePath)
                 val protoFieldNumber = parsedProtoDescriptorMap[absolutePath]
                 aux.validated = protoFieldNumber?.let { Proto3MetaModelMapValidated(absolutePath, it) }
             } else {
-                val fullName = leaderFieldMeta1.getAux<Resolved>(RESOLVED_AUX)?.fullName
+                val fullName = childMeta.getAux<Resolved>(RESOLVED_AUX)?.fullName
                 errors.add("$fullName mapping $absolutePath does not exist")
             }
         }
@@ -55,7 +70,13 @@ private class ValidateProto3MetaModelMapVisitor(
         val protoDescriptorMap = HashMap<String, Int>()
         val fileDescriptorSet = FileDescriptorSet.parseFrom(FileInputStream(protoDescriptorFile))
         for (protoFile in fileDescriptorSet.fileList) {
+            collectProtoEnums(protoDescriptorMap, "${protoFile.name}:", protoFile.enumTypeList)
             for (protoMessage in protoFile.messageTypeList) {
+                collectProtoEnums(
+                    protoDescriptorMap,
+                    "${protoFile.name}:/${protoMessage.name}",
+                    protoMessage.enumTypeList
+                )
                 for (protoMessageField in protoMessage.fieldList) {
                     val path = "${protoFile.name}:/${protoMessage.name}/${protoMessageField.name}"
                     protoDescriptorMap.putIfAbsent(path, protoMessageField.number)
@@ -63,5 +84,18 @@ private class ValidateProto3MetaModelMapVisitor(
             }
         }
         return protoDescriptorMap
+    }
+
+    private fun collectProtoEnums(
+        protoDescriptorMap: HashMap<String, Int>,
+        pathPrefix: String,
+        enumTypeList: List<DescriptorProtos.EnumDescriptorProto>
+    ) {
+        for (protoEnum in enumTypeList) {
+            for (protoEnumValue in protoEnum.valueList) {
+                val path = "$pathPrefix/${protoEnum.name}/${protoEnumValue.name}"
+                protoDescriptorMap.putIfAbsent(path, protoEnumValue.number)
+            }
+        }
     }
 }
--- src/main/kotlin/org/treeWare/proto3/validation/ValidateProto3Path.kt
+++
@@ -1,2 +0,0 @@
-package org.treeWare.proto3.validation
-
