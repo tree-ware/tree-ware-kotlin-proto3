6d47e376956465feeecc4ad959417dbea09837b6
Deepak Nulu <deepak@celona.io>
2022-02-08 22:36:19 -0800

    Switch to multiplatform core and text-fixtures

--- src/main/kotlin/org/treeWare/proto3/aux/Proto3MetaModelMapStateMachine.kt
+++ src/main/kotlin/org/treeWare/proto3/aux/Proto3MetaModelMapStateMachine.kt
@@ -3,6 +3,7 @@ package org.treeWare.proto3.aux
 import org.treeWare.model.decoder.stateMachine.AbstractDecodingStateMachine
 import org.treeWare.model.decoder.stateMachine.AuxDecodingStateMachine
 import org.treeWare.model.decoder.stateMachine.DecodingStack
+import org.treeWare.util.assertInDevMode
 
 class Proto3MetaModelMapStateMachine(
     private val stack: DecodingStack
@@ -29,19 +30,19 @@ class Proto3MetaModelMapStateMachine(
 
     override fun decodeObjectEnd(): Boolean {
         // Remove self from stack
-        stack.pollFirst()
+        stack.removeFirst()
         return true
     }
 
     override fun decodeListStart(): Boolean {
         // This method should never get called
-        assert(false)
+        assertInDevMode(false)
         return false
     }
 
     override fun decodeListEnd(): Boolean {
         // This method should never get called
-        assert(false)
+        assertInDevMode(false)
         return false
     }
 }
